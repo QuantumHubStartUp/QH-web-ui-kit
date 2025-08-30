300ffa80df7f4f214568b35370ac24ba5ac5f6d6
Freyzan2006 <freyzan2006@gmail.com>
2025-08-30 22:48:07 +0500

    добавил button и checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import "./styles.css";
 
-export * from './Button';
+// export * from './Button';
+// export * from "./Loader";
+
+export { Button } from "./ui/Button";
+export { Loader } from "./ui/Loader";
+export { Checkbox } from "./ui/Checkbox";
