9f8d27cb085348acf7790d738c2ce50a4bd49cf7
Freyzan2006 <freyzan2006@gmail.com>
2025-10-11 12:51:35 +0500

    updated component Card.tsx

--- src/ui/Card/index.tsx
+++ src/ui/Card/index.tsx
@@ -3,8 +3,9 @@
 import React from 'react';
 import { type VariantProps } from 'class-variance-authority';
 import { cardVariants } from './variations';
-
-
+import { CardHead } from './CardHead';
+import { CardFooter } from './CardFooter';
+import { CardBody } from './CardBody';
 
 interface CardProps 
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -12,23 +13,23 @@ interface CardProps
   className?: string;
 }
 
+const Card: React.FC<CardProps> & {
+  Header: typeof CardHead;
+  Body: typeof CardBody;
+  Footer: typeof CardFooter;
+} = ({ className, variant, padding, hover, ...props }) => {
+  return (
+    <div
+      className={cardVariants({ variant, padding, hover, className })}
+      {...props}
+    />
+  );
+};
 
-export const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ className, variant, padding, hover, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cardVariants({ variant, padding, hover, className })}
-        {...props}
-      />
-    );
-  }
-);
-
+Card.Header = CardHead;
+Card.Body = CardBody;
+Card.Footer = CardFooter;
 Card.displayName = 'Card';
 
-
-
-
-
-
+export { Card };
+export default Card;
